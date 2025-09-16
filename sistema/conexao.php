b99c00841bd0782e76eb8844dc9d67c697b542ef
LUCAS KRETSCHMAR <lucas15kretschmar@gmail.com>
2025-09-16 16:00:59 -0300

    arquivo funcionando

--- sistema/conexao.php
+++ sistema/conexao.php
@@ -1,3 +1,12 @@
 <?php
+$host = "localhost";
+$banco = "escola_tecnica";
+$user = "root";
+$senha = "";
 
-?>
+try {
+    $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $banco . ';charset=utf8;', $user, $senha);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("erro ao conectar: " . $e->getMessage());
+}
