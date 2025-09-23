b5d6d8480b8e78ec425c76cf78f36f295048861e
kretschmar-git <lucas15kretschmar@gmail.com>
2025-09-23 17:48:07 -0300

    adicionado correção para excluir matriculas

--- sistema/excluir.php
+++ sistema/excluir.php
@@ -3,8 +3,14 @@
 
     $id = $_POST['id'];
 
-    $sql = $pdo->prepare("DELETE FROM Aluno WHERE id = ?");
+    //apagar matricula se existir
+    $sql =  $pdo->prepare("DELETE FROM matricula WHERE aluno = ?");
+    $sql->execute([$id]);
 
+
+
+    //apagar o aluno
+    $sql = $pdo->prepare("DELETE FROM Aluno WHERE id = ?");
     $sql->execute([$id]);
 
     header("Location: index.php");
